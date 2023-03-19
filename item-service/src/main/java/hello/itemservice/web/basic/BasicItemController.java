d49aee03070dcdebe07abf066b17db3ac682399b
Junyoung <wnsdud12365@naver.com>
2023-03-19 20:24:09 +0900

    [Add] 상품 등록 컨트롤러 + @ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,10 +5,7 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -40,9 +37,38 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    // @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName, @RequestParam Integer price, @RequestParam Integer quantity, Model model){
+        Item item = new Item(itemName, price, quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
+
+        /*
+        * model.addAttribute("item", item);
+        * @ModelAttribute("name") 은 model 에 동일한 이름(Key)으로 받은 데이터를 넣어준다.
+        * 때문에 윗 문장이 주석처리되어도 되는 것! + 파라미터로 모델 받을 필요도 없음!
+        */
+        return "basic/item";
+    }
+
+    // @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){
+        itemRepository.save(item);
+        // Name 속성을 넣지 않으면 Class 의 맨 첫글자를 소문자로 바꾸어 ModelAttribute 에 담긴다.
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save(){
-        return "basic/addForm";
+    public String addItemV4(Item item){
+        // 기본형이나 String 이 아닌 객체가 오면 ModelAttribute 를 생략해도 된다!
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     @PostConstruct
